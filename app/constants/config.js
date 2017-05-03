8541c60eebd8bc1ac89a84689241b379259a64cb
Nhat Dao <daoanhnhat1995@gmail.com>
2017-05-03 11:34:03 -0500

    update config

--- app/constants/config.js
+++ app/constants/config.js
@@ -2,7 +2,7 @@ export const MAX_THREAD_NUMBER = 30;
 export const HN_API_URL = 'https://hacker-news.firebaseio.com/v0/'; 
 export const PAGE_SIZE = 5;
 // export const OPENFACE_SOCKET_ADDRESS ="ws://52.42.211.186:9000";
-export const EVENT_SERVER_SOCKET_ADDRESS = "ws://52.42.211.186:9001/ws";
+export const EVENT_SERVER_SOCKET_ADDRESS = process.env.NODE_ENV == 'production' ? 'ws://52.42.211.186:9001/ws' : "ws://192.168.99.100:9001/ws"
 export const OPENFACE_SOCKET_ADDRESS= process.env.NODE_ENV == 'production' ? "ws://52.42.211.186:9000" : "ws://192.168.99.100:9000";
 export const WEATHER_API_ENDPOINT = "https://api.darksky.net/forecast/9e1bfc49cdc03b377f4d00753ff13ada/";
 export const DEFAULT_LAT = "32.7357";
