c93b4c203a5f482283451dc3285f25e7c007bda2
Nhat Dao <daoanhnhat1995@gmail.com>
2017-05-05 08:57:11 -0500

    fixed

--- app/constants/config.js
+++ app/constants/config.js
@@ -7,6 +7,7 @@ export const PAGE_SIZE = 5;
 export const EVENT_SERVER_SOCKET_ADDRESS = "ws://184.73.147.177:9001/ws";
 //export const EVENT_SERVER_SOCKET_ADDRESS = "ws://localhost:9001/ws";
 export const OPENFACE_SOCKET_ADDRESS = "ws://184.73.147.177:9000";
+// export const OPENFACE_SOCKET_ADDRESS="ws://localhost:9000";
 export const WEATHER_API_ENDPOINT = "https://api.darksky.net/forecast/9e1bfc49cdc03b377f4d00753ff13ada/";
 export const DEFAULT_LAT = "32.7357";
 export const DEFAULT_LNG = "-97.1081";
